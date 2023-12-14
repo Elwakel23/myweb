1e7f6a13faf1d17458841a8d4300105874019146
Elwakel23 <149334979+Elwakel23@users.noreply.github.com>
2023-12-14 18:40:10 -0500

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+    function downloadJSAtOnload(){var d=document.createElement("script");d.src="https://cdn.jsdelivr.net/gh/Arlina-Design/redvision@master/htmlparse.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+    //]]>
