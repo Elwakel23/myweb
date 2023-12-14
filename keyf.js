4ccef83ecb8c186177d0cbea8c86a8fb17d8c853
Elwakel23 <149334979+Elwakel23@users.noreply.github.com>
2023-12-14 18:49:58 -0500

    Create keyf.js

---
+++ keyf.js
@@ -0,0 +1,16 @@
+//Don't remove any script If you will remove it then this will not work
+var _0xdde9 = [ "setInterval", "click", "hidden", "removeClass", ".hidden", "val", "input[name=MethodOptions]:checked", "on", "#startjob", "html", "#lang", "lang", "data", "#gg-lang", ".select-lang li", "display", "css", "#credits", "none", "visibility", ":visible", "is", ":hidden", "style", "credits", "getElementById", "inherit", "inline", "https://infiniteinsightsonline.blogspot.com/", "assign", "location", "length", "#creditlink", "href", "attr", "https://infiniteinsightsonline.blogspot.com/", "text", "Infinite Insights Online" ];
+
+window[_0xdde9[0]]($().DoJob, 750);
+
+$(_0xdde9[8])[_0xdde9[7]](_0xdde9[1], function() {
+    $(_0xdde9[4])[_0xdde9[3]](_0xdde9[2]);
+    if (1 == $(_0xdde9[6])[_0xdde9[5]]()) $().StartJobMethod1(); else $().StartJobMethod2();
+});
+
+$(_0xdde9[14])[_0xdde9[7]](_0xdde9[1], function() {
+    $(_0xdde9[10])[_0xdde9[9]]($(this)[_0xdde9[9]]());
+    $(_0xdde9[13])[_0xdde9[5]]($(this)[_0xdde9[12]](_0xdde9[11]));
+});
+
+var _0x6c23 = [ _0xdde9[15], _0xdde9[16], _0xdde9[17], _0xdde9[18], _0xdde9[19], _0xdde9[2], _0xdde9[20], _0xdde9[21], _0xdde9[22], _0xdde9[23], _0xdde9[24], _0xdde9[25], _0xdde9[26], _0xdde9[27], _0xdde9[28], _0xdde9[29], _0xdde9[30], _0xdde9[31], _0xdde9[32], _0xdde9[33], _0xdde9[34], _0xdde9[35], _0xdde9[36], _0xdde9[37] ];
